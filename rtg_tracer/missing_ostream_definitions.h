468241afd19e9f354152d59ba04d0b9a73e9586c
Jeff Daily <jeff.daily@amd.com>
2021-10-08 19:13:12 +0000

    missing ostreams and HSA signature changes for rocm 4.5

--- rtg_tracer/missing_ostream_definitions.h
+++ rtg_tracer/missing_ostream_definitions.h
@@ -1,77 +1,45 @@
 #include <iostream>
 #include <sstream>
 
-std::ostream& operator<<(std::ostream& out, const HIP_MEMCPY3D obj) {
-    return out << "HIP_MEMCPY3D";
+#define DUMMY(T)  \
+std::ostream& operator<<(std::ostream& out, const T obj) { \
+    return out << #T; \
 }
 
-std::ostream& operator<<(std::ostream& out, const hip_Memcpy2D obj) {
-    return out << "hip_Memcpy2D";
-}
+DUMMY(HIP_MEMCPY3D)
+DUMMY(hip_Memcpy2D)
+DUMMY(hipPitchedPtr)
+DUMMY(hipExtent)
+DUMMY(hipArray)
+DUMMY(hipLaunchParams)
+DUMMY(hipChannelFormatDesc)
+DUMMY(HIP_ARRAY_DESCRIPTOR)
+DUMMY(hipDeviceProp_t)
+DUMMY(hipIpcEventHandle_t)
+DUMMY(hipIpcMemHandle_t)
+DUMMY(hipMemcpy3DParms)
+DUMMY(HIP_ARRAY3D_DESCRIPTOR)
+DUMMY(hipFuncAttributes)
+DUMMY(hipPointerAttribute_t)
 
 std::ostream& operator<<(std::ostream& out, const dim3 obj) {
     return out << "{" << obj.x << "," << obj.y << "," << obj.z << "}";
 }
 
-std::ostream& operator<<(std::ostream& out, const hipPitchedPtr obj) {
-    return out << "hipPitchedPtr";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipExtent obj) {
-    return out << "hipExtent";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipArray obj) {
-    return out << "hipArray";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipLaunchParams obj) {
-    return out << "hipLaunchParams";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipChannelFormatDesc obj) {
-    return out << "hipChannelFormatDesc";
-}
-
-std::ostream& operator<<(std::ostream& out, const HIP_ARRAY_DESCRIPTOR obj) {
-    return out << "HIP_ARRAY_DESCRIPTOR";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipDeviceProp_t obj) {
-    return out << "hipDeviceProp_t";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipIpcEventHandle_t obj) {
-    return out << "hipIpcEventHandle_t";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipIpcMemHandle_t obj) {
-    return out << "hipIpcMemHandle_t";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipMemcpy3DParms obj) {
-    return out << "hipMemcpy3DParms";
-}
-
-std::ostream& operator<<(std::ostream& out, const HIP_ARRAY3D_DESCRIPTOR obj) {
-    return out << "HIP_ARRAY3D_DESCRIPTOR";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipFuncAttributes obj) {
-    return out << "hipFuncAttributes";
-}
-
-std::ostream& operator<<(std::ostream& out, const hipPointerAttribute_t obj) {
-    return out << "hipPointerAttribute_t";
-}
-
 #if (HIP_VERSION_MAJOR == 4 && HIP_VERSION_MINOR >= 3) || HIP_VERSION_MAJOR > 4
-std::ostream& operator<<(std::ostream& out, const hipExternalMemoryHandleDesc obj) {
-    return out << "hipExternalMemoryHandleDesc";
-}
+DUMMY(hipExternalMemoryHandleDesc)
+DUMMY(hipExternalMemoryBufferDesc)
+#endif
 
-std::ostream& operator<<(std::ostream& out, const hipExternalMemoryBufferDesc obj) {
-    return out << "hipExternalMemoryBufferDesc";
-}
+#if (HIP_VERSION_MAJOR == 4 && HIP_VERSION_MINOR >= 4) || HIP_VERSION_MAJOR > 4
+DUMMY(hipResourceDesc)
+DUMMY(hipKernelNodeParams)
+DUMMY(hipExternalSemaphoreWaitParams)
+DUMMY(hipMemsetParams)
+DUMMY(hipExternalSemaphoreHandleDesc)
+DUMMY(hipExternalSemaphoreSignalParams)
+DUMMY(textureReference)
+DUMMY(hipMipmappedArray)
+#define HIP_API_ID_NUMBER HIP_API_ID_LAST
 #endif
 
